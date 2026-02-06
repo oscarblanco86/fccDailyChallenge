b12bd97e77e140770ab51bfc1bdbff4fa25b6da3
Oscar Blanco <oscarmb86@hotmail.com>
2026-02-06 00:17:41 -0600

    finished the 28 exercise

---
+++ dailyChallenge/python/2026/01/28.py
@@ -0,0 +1,32 @@
+# Flatten the Array
+
+# Given an array that contains nested arrays, return a new array with all values flattened into a single, one-dimensional array. Retain the original order of the items in the arrays.
+
+
+# 1. flatten([1, [2, 3], 4]) should return [1, 2, 3, 4].
+# 2. flatten([5, [4, [3, 2]], 1]) should return [5, 4, 3, 2, 1].
+# 3. flatten(["A", [[[["B"]]]], "C"]) should return ["A", "B", "C"].
+# 4. flatten([["L", "M", "N"], ["O", ["P", "Q", ["R", ["S", ["T", "U"]]]]], "V", ["W", ["X", ["Y", ["Z"]]]]]) should return ["L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"].
+# 5. flatten([["red", ["blue", ["green", ["yellow", ["purple"]]]]], "orange", ["pink", ["brown"]]]) should return ["red","blue","green","yellow","purple","orange","pink","brown"].
+
+
+def items_on_array(arr):
+    new_arr = []
+    for item in arr:
+        if type(item) is list:
+            for inner_item in items_on_array(item):
+                new_arr.append(inner_item)
+        else:
+            new_arr.append(item)
+    return new_arr
+
+def flatten(arr):
+    return items_on_array(arr)
+
+
+
+print(flatten([1, [2, 3], 4]))
+print(flatten([5, [4, [3, 2]], 1]))
+print(flatten(["A", [[[["B"]]]], "C"]))
+print(flatten([["L", "M", "N"], ["O", ["P", "Q", ["R", ["S", ["T", "U"]]]]], "V", ["W", ["X", ["Y", ["Z"]]]]]))
+print(flatten([["red", ["blue", ["green", ["yellow", ["purple"]]]]], "orange", ["pink", ["brown"]]]))
